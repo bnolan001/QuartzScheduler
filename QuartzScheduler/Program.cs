5d5c1be182739b1575a0c1968dafa9541fd56c14
Brian Nolan <bnolan001@gmail.com>
2022-11-26 20:08:03 -0500

    Updated comments and removed test code

--- QuartzScheduler/Program.cs
+++ QuartzScheduler/Program.cs
@@ -15,12 +15,9 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 builder.Host.UseSerilog();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddQuartzScheduler(builder.Configuration);
-
 
 var app = builder.Build();
 
